eab2f1f0a169aabc0196e4072f31b1bd1eaae9ce
rayaneBouzroura <2111752@cegepmontpetit.ca>
2024-09-08 11:18:46 -0400

    game logic works , random 2d bit array that goes through different generations

--- game-of-life/src/app/game/game.component.ts
+++ game-of-life/src/app/game/game.component.ts
@@ -46,38 +46,118 @@ export class GameComponent implements OnInit, AfterViewInit {
 
     const gameBoard = this.setupGrid(numOfRows, numOfColumns);
     this.render(gameBoard);
+
+
+    this.animate(gameBoard);
+  }
+  animate(gameBoard: BitArray[]) {
+    requestAnimationFrame(() => this.animate(this.createNextGen(gameBoard)));
+    this.render(gameBoard);
   }
 
   setupGrid(numOfRows: number, numOfColumns: number) {
-    const binaryRandom = () => Math.random() > 0.5 ? 1 : 0;
-
+    const binaryRandom = () => (Math.random() > 0.5 ? 1 : 0);
 
     const board: BitArray[] = new Array(numOfRows)
       .fill(0)
       .map(() => new Array(numOfColumns).fill(0).map(binaryRandom));
 
-
     console.log(board);
 
     return board;
-
-
   }
 
   render(gameBoard: BitArray[]) {
     const ctx = this.context;
     const res = RESOLUTION;
-    gameBoard.forEach((row,rowIndex) =>{
-      row.forEach((c , collIndex) =>{
+    gameBoard.forEach((row, rowIndex) => {
+      row.forEach((c, collIndex) => {
         ctx!.beginPath();
-        ctx!.rect(collIndex * res , rowIndex * res , res , res);
+        ctx!.rect(collIndex * res, rowIndex * res, res, res);
         ctx!.fillStyle = c ? 'black' : 'white';
         ctx!.fill();
-        ctx!.stroke();
-      })
-    })
+        //ctx!.stroke();
+      });
+    });
   }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
+
+  private createNextGen(gameBoard: BitArray[]): BitArray[] {
+    const nextGen: BitArray[] = [];
+    //copy current state of board
+    //not efficent mais bon
+    for (let i = 0; i < gameBoard.length; i++) {
+      nextGen[i] = [...gameBoard[i]];
+    }
+
+    gameBoard.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        //FIRST STEP
+        //RETRIEVE ALL 8 NEIGHBORS OF A CELL
+        //MAIS : CORNER CELL HAVE ONLY 3 NEIGHBORS
+        //AND EDGE CELLS ONLY HAVE 5 NEIGHBORS
+        //AND WE SHOULDNT CONSIDER THE ABSENCE OF THE EXTRA CELLS AS A DEAD NEIGHBOR
+
+        //rules :
+
+        //1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        //2. Any live cell with two or three live neighbours lives on to the next generation.
+        //3. Any dead cell with EXACLTLY three live neighbours become a live cell, as if by reproduction (segs)
+
+        //pseudo logic
+        // check if cell gonna be alive next iter
+        const aliveNeighbors: Number = countOfLivingNeighbors(
+          gameBoard,
+          rowIndex,
+          colIndex
+        );
+        const becomeEnVie = aliveNeighbors === 3;
+        //check if cell will STAY alive next iter , check that its alive first then check that it has exactly 2 neighbors
+        const resteEnVie = cell && aliveNeighbors === 2;
+        const newCellValue = becomeEnVie || resteEnVie ? 1 : 0;
+
+        nextGen[rowIndex][colIndex] = newCellValue;
+      });
+    });
+
+    return nextGen;
+  }
+}
+
+function countOfLivingNeighbors(
+  gameBoard: BitArray[],
+  rowIndex: number,
+  colIndex: number
+): Number {
+  //TL | T | TR
+  //---------
+  //L  | C | R
+  //---------
+  //BL | B | BR
+
+  //edge cases (hehe)
+  const TL = gameBoard[rowIndex - 1]?.[colIndex - 1];
+  const T = gameBoard[rowIndex - 1]?.[colIndex];
+  const TR = gameBoard[rowIndex - 1]?.[colIndex + 1];
+
+  const L = gameBoard[rowIndex]?.[colIndex - 1];
+  const R = gameBoard[rowIndex]?.[colIndex + 1];
+
+  const BL = gameBoard[rowIndex + 1]?.[colIndex - 1];
+  const B = gameBoard[rowIndex + 1]?.[colIndex];
+  const BR = gameBoard[rowIndex + 1]?.[colIndex + 1];
+
+  let aliveNeighbors = 0;
+
+  if (TL !== undefined && TL === 1) aliveNeighbors++;
+  if (T !== undefined && T === 1) aliveNeighbors++;
+  if (TR !== undefined && TR === 1) aliveNeighbors++;
+  if (L !== undefined && L === 1) aliveNeighbors++;
+  if (R !== undefined && R === 1) aliveNeighbors++;
+  if (BL !== undefined && BL === 1) aliveNeighbors++;
+  if (B !== undefined && B === 1) aliveNeighbors++;
+  if (BR !== undefined && BR === 1) aliveNeighbors++;
+  return aliveNeighbors;
 }
